df11db71989a234bd5eddcaf6f88ebdd903b92f8
Akash Francis <akashfrancis3211@gmail.com>
2026-04-09 07:57:22 +0000

    This is my chef commit for apache server

---
+++ book1/recipes/apache-server.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: book1
+# Recipe:: apache-server
+#
+# Copyright:: 2026, The Authors, All Rights Reserved.
+file "/apache_server" do
+  content "This is for hosting Apache Server by Akash Francis"
+  action :create
+end
---
+++ book1/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: book1
+# Recipe:: default
+#
+# Copyright:: 2026, The Authors, All Rights Reserved.
